952fba2ac0e2433a6a87070f571ff9762e8da60b
Gbiterium <samueladetona56@gmail.com>
2023-10-14 23:31:33 +0100

    WIP

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,7 +1,7 @@
 export default function ({store, redirect, app }) {
     if (!store.getters['auth/isLoggedIn']) {
         if (process.client) {
-        app.$toast('Unauthorized, please login')
+        app.$toast({type: 'error', text: 'Unauthorized, please login'})
         }
         return redirect('/auth/login')
     }
