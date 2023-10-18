81dbfef3a7b1d6c0461c8bda638dedf0564889f0
Gbiterium <samueladetona56@gmail.com>
2023-10-18 16:04:45 +0100

    fixes

--- pages/parent/_school/exercises/index.vue
+++ pages/parent/_school/exercises/index.vue
@@ -54,8 +54,13 @@ export default {
     methods: {
         async getAssignments(status) {
             try {
+                const token = this.$cookies.get('auth-token')
                 this.loading = true
-                const { data } = await this.$axios.get(`/util/v2/mobile/exercises/${this.$route.query.student_id}?page_size=12&status=${status}`)
+                const { data } = await this.$axios.get(`/util/v2/mobile/exercises/${this.$route.query.student_id}?page_size=12&status=${status}`, {
+                    headers: {
+              Authorization: `Bearer ${this.token}`,
+            },
+                })
                 this.exercises = data.data.results
             } catch (error) {
                 console.log(error)
